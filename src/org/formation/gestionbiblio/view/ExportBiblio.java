dde4b3a23049d8344d0fda6b7a938c28feb22c72
remimassenya <remi.massenya@hotmail.fr>
2019-11-14 13:59:12 +0100

    Export word maj

--- src/org/formation/gestionbiblio/view/ExportBiblio.java
+++ src/org/formation/gestionbiblio/view/ExportBiblio.java
@@ -18,26 +18,23 @@ import javafx.css.ParsedValue;
 public class ExportBiblio {
 	//Blank Document
     XWPFDocument document;
-	XWPFParagraph paragraph;
+	XWPFParagraph paragraph_header;
+	XWPFParagraph paragraph_titre;
+	XWPFParagraph paragraph_sommaire;
+	XWPFParagraph paragraph_livres;
+	XWPFParagraph paragraph_pret;
     XWPFRun sommaire;
     XWPFRun header;
-    XWPFRun sautpage;
+    XWPFRun title;
     XWPFRun titre;
     XWPFRun livres;
     XWPFTable table;
     XWPFTableRow tableligne1;
     XWPFTableRow tableligne2;
-    String filename ="bibliocr";
+    String filename ="bibliotheque";
     
     public ExportBiblio() {
-    	document = new XWPFDocument(); 
-    	paragraph = document.createParagraph();
-    	header = paragraph.createRun();
-    	titre = paragraph.createRun();
-    	sautpage = paragraph.createRun();
-    	sommaire = paragraph.createRun();
-    	sautpage = paragraph.createRun();
-    	livres = paragraph.createRun();
+    	
     }
     
 	public void exportFile() throws IOException{
@@ -45,11 +42,65 @@ public class ExportBiblio {
     		
     	      	//Write the Document in file system
     	      	FileOutputStream out = new FileOutputStream(new File(filename+".docx"));
-    	      
-    	      	entete();		//appel du texte a ecrire dans le document
+	    	      	document = new XWPFDocument(); 
+			    	
+			    	//paragraph_pret = document.createParagraph();
+
+			    	paragraph_header = document.createParagraph();
+			    	header = paragraph_header.createRun();
+			    	header.setFontSize((short)(18));
+					header.setText(LocalDate.now() +" "+filename);
+					
+					
+			    	paragraph_titre = document.createParagraph();
+			    	titre = paragraph_titre.createRun();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.addCarriageReturn();
+			    	titre.setText("CONTENU DE LA BIBLIOTHEQUE");
+					titre.addBreak(BreakType.PAGE);
+			    	
+			    	paragraph_sommaire = document.createParagraph();
+			    	sommaire = paragraph_sommaire.createRun();
+			    	sommaire.setText("Sommaire");
+					
+					paragraph_livres = document.createParagraph();
+			    	livres = paragraph_livres.createRun();
+			    	int i;
+					String text="";
+					i=0;
+					while(i<BiblioService.getInstance().getBiblio().getLivre().size())
+					{		
+							String parutionStr = Integer.toString(BiblioService.getInstance().getBiblio().getLivre().get(i).getParution());
+							String rangeeStr = Integer.toString(BiblioService.getInstance().getBiblio().getLivre().get(i).getRangee());
+							String colonneStr = Integer.toString(BiblioService.getInstance().getBiblio().getLivre().get(i).getColonne());
+											
+							text =text+"NOM DU LIVRE:"+BiblioService.getInstance().getBiblio().getLivre().get(i).getTitre()+" AUTEUR:"+BiblioService.getInstance().getBiblio().getLivre().get(i).getAuteur().getNom()+" DESCRIPTION:"+BiblioService.getInstance().getBiblio().getLivre().get(i).getPresentation()+" PARUTION:"+parutionStr+" RANGEE:"+rangeeStr+" COLONNE:"+colonneStr+"IMAGE URL"+BiblioService.getInstance().getBiblio().getLivre().get(i).getImgUrl();
+							i++;
+					 } 
+					livres.setText(text);
+					
+					
+//    	      	entete();		//appel du texte a ecrire dans le document
+					
+					
     	      
     	      	document.write(out);
     	      	out.close();
+    	      	document.close();
     	      	System.out.println("DOCUMENT CREE");
         }
     	catch(IOException e)
@@ -60,25 +111,6 @@ public class ExportBiblio {
 	
 	private void entete() throws IOException{
     	
-		int i;
 		
-		header.setFontSize((short)(18));
-		header.setText(LocalDate.now() +" "+filename);
-		titre.setText("BIBLIOTHEQUE");
-		sautpage.addBreak(BreakType.PAGE);
-		sommaire.setText("Sommaire");
-		sautpage.addBreak(BreakType.PAGE);
-		String text="";
-		i=0;
-		while(i<BiblioService.getInstance().getBiblio().getLivre().size())
-		{		
-				String parutionStr = Integer.toString(BiblioService.getInstance().getBiblio().getLivre().get(i).getParution());
-				String rangeeStr = Integer.toString(BiblioService.getInstance().getBiblio().getLivre().get(i).getRangee());
-				String colonneStr = Integer.toString(BiblioService.getInstance().getBiblio().getLivre().get(i).getColonne());
-								
-				text =text+"NOM DU LIVRE:"+BiblioService.getInstance().getBiblio().getLivre().get(i).getTitre()+" AUTEUR:"+BiblioService.getInstance().getBiblio().getLivre().get(i).getAuteur().getNom()+" DESCRIPTION:"+BiblioService.getInstance().getBiblio().getLivre().get(i).getPresentation()+" PARUTION:"+parutionStr+" RANGEE:"+rangeeStr+" COLONNE:"+colonneStr+"IMAGE URL"+BiblioService.getInstance().getBiblio().getLivre().get(i).getImgUrl();
-				i++;
-		 } 
-		livres.setText(text);
     }
 }
